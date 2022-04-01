f788317465b9167cc574c23c2db5b43271b2b709
Andrew Belcher <belcher.w.andrew@gmail.com>
2022-04-01 12:22:20 +0800

    Completed soduku-solver.js checks

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,18 +1,90 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    let beginIndex = row.charCodeAt(0) - 65;
+    let endIndex = beginIndex + 9;
 
+    let r = puzzleString.slice(beginIndex, endIndex);
+
+    let rowRegex = new RegExp(value);
+    
+    if(!rowRegex.test(r)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    let puzzleArray = puzzleString.split('');
 
+    let cArray = [];
+
+    for(let i = 0; i < 9; i++) {
+      cArray.push(puzzleArray[i * 9 + parseInt(column) - 1]);
+    }
+
+    if(!cArray.includes(value)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    //Find region
+    let rRegion;
+    let cRegion;
 
+    switch(true) {
+      case (row == 'A' || row == 'B' || row == 'C'):
+        rRegion = 0;
+        break;
+      case (row == 'D' || row == 'E' || row == 'F'):
+        rRegion = 1;
+        break;
+      case (row == 'G' || row == 'H' || row == 'I'):
+        rRegion = 2;
+        break;
+    }
+
+    switch(true) {
+      case (column == '1' || column == '2' || column == '3'):
+        cRegion = 0;
+        break;
+      case (column == '4' || column == '5' || column == '6'):
+        cRegion = 1;
+        break;
+      case (column == '7' || column == '8' || column == '9'):
+        cRegion = 2;
+        break;
+    }
+
+    //
+    let puzzleArray = puzzleString.split('');
+
+    let regionArray = [];
+
+    for(let i = 0; i < 3; i++) {
+      for(let j = 0; j < 3; j++) {
+        regionArray.push(puzzleArray[(i + (rRegion * 27)) + (j * 9 + (cRegion * 3))]);
+      }
+    }
+
+    return regionArray;
+
+    if(!regionArray.includes(value)) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   solve(puzzleString) {
--- routes/api.js
+++ routes/api.js
@@ -8,12 +8,83 @@ module.exports = function (app) {
 
   app.route('/api/check')
     .post((req, res) => {
+      let puzzle = req.body.puzzle;
+      let coordinate = req.body.coordinate;
+      let value = req.body.value;
 
+      //#11 Check for missing puzzle, coordinate or value
+      if(!puzzle || !coordinate || !value) {
+        console.log({error: 'Required field(s) missing'});
+        return res.json({error: 'Required field(s) missing'});
+      }
+
+      //#9 Check for invalid characters in puzzle
+      let puzzleRegex = /^[\.1-9]+$/;
+
+      if(!puzzleRegex.test(puzzle)) {
+        console.log({error: 'Invalid characters in puzzle'});
+        return res.json({error: 'Invalid characters in puzzle'});
+      }
+
+      //#10 Check for 81 characters
+      if(puzzle.length != 81) {
+        console.log({error: 'Expected puzzle to be 81 characters long'});
+        return res.json({error: 'Expected puzzle to be 81 characters long'});
+      }
+
+      //#12 Check for invalid coordinate
+      let coordinateRegex = /^[A-I][1-9]$/;
+
+      if(!coordinateRegex.test(coordinate)) {
+        console.log({error: 'Invalid coordinate'});
+        return res.json({error: 'Invalid coordinate'});
+      }
+
+      //#13 Check value is valid number
+      let valueRegex = /^[1-9]$/;
+
+      if(!valueRegex.test(value)) {
+        console.log({error: 'Invalid value'});
+        return res.json({error: 'Invalid value'});
+      }
+
+      //#6-8 Handle checking input
+      // console.log(solver.checkRowPlacement(puzzle, coordinate[0], coordinate[1], value));
+      // console.log(solver.checkColPlacement(puzzle, coordinate[0], coordinate[1], value));
+      console.log(solver.checkRegionPlacement(puzzle, coordinate[0], coordinate[1], value));
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      let puzzle = req.body.puzzle;
 
+      //Testing
+      console.log(puzzle);
+      // solver.validate(puzzle);
+
+      //#2 Check for missing puzzle
+      if(!puzzle) {
+        console.log({error: 'Required field missing'});
+        return res.json({error: 'Required field missing'});
+      }
+      
+      //#3 Check for invalid characters
+      let puzzleRegex = /^[\.1-9]+$/;
+
+      if(!puzzleRegex.test(puzzle)) {
+        console.log({error: 'Invalid characters in puzzle'});
+        return res.json({error: 'Invalid characters in puzzle'});
+      }
+
+      //#4 Check for 81 characters
+      if(puzzle.length != 81) {
+        console.log({error: 'Expected puzzle to be 81 characters long'});
+        return res.json({error: 'Expected puzzle to be 81 characters long'});
+      }
+
+      //#5 If puzzle cannot be solved
+      console.log({error: 'Puzzle cannot be solved'});
+      return res.json({error: 'Puzzle cannot be solved'});
     });
 };
 
